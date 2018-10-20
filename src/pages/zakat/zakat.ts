a9c0e4ed4df63e17a2b2a3d50787aa46cb7a95b1
SD95 <abckonefsidy95@gmail.com>
2018-10-20 14:38:09 +0000

    Ajout du component pour la zakat

---
+++ src/pages/zakat/zakat.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'page-zakat',
+    templateUrl: 'zakat.html'
+})
+
+export class ZakatPage {
+    constructor() {
+        
+    }
+}
