a68d689ff4c5a77a60b291147595fb038ef878f3
abdoulayeNDAO12 <abdoulayendao12@gmail.com>
2018-10-23 22:27:27 +0000

    modif de la page accueil et changement des identifiants de la page de connexion

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 import { AccueilPage } from '../accueil/accueil';
+import { NotificationPage } from '../notification/notification';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -12,6 +13,7 @@ export class TabsPage {
   tab1Root = AccueilPage;
   tab2Root = AboutPage;
   tab3Root = ContactPage;
+  tab4Root = NotificationPage;
 
   constructor() {
 
